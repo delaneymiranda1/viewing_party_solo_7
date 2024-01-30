3059cb39e589a99cf5b279b03acd41edcd3475f9
Miranda Delaney <delaney.miranda1@gmail.com>
2024-01-30 14:19:48 -0700

    user story one complete minus sad path testing

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   root "welcome#index"
   get '/register', to: 'users#new', as: 'register_user'
 
+
   resources :users, only: [:show, :create] do
+    resources :discover, only: [:index]
+    resources :movies, only: [:index]
   end
 end
--- spec/features/users/discover/index_spec.rb
+++ spec/features/users/discover/index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'User Discover Movies', type: :feature do
   before(:each) do
-    
+    @user1 = User.create!(name: 'Joe', email: 'joe@gmail.com')
   end
 
  # US 1
@@ -10,11 +10,29 @@ RSpec.describe 'User Discover Movies', type: :feature do
     it 'I see a Button to Discover Top Rated Movies,
       a text field to enter keyword(s) to search by movie title,
       and a Button to Search by Movie Title' do
-
-
+      visit user_discover_index_path(@user1)
+      save_and_open_page
+      expect(page).to have_button("Discover Top Rated Movies")
+      expect(page).to have_content("Search by movie title:")
+      expect(page).to have_field(:title)
+      expect(page).to have_button("Search")
     end
 
-    it '#sadpath - an invalid user id does not work' do
+    it 'when you click top rated movies button you are taken to the movie results page' do
+      visit user_discover_index_path(@user1)
+
+      click_button("Discover Top Rated Movies")
+      expect(current_path).to eq(user_movies_path(@user1))
+    end
+
+    it 'when you click search button you are taken to the movie results page' do
+      visit user_discover_index_path(@user1)
+
+      click_button("Search")
+      expect(current_path).to eq(user_movies_path(@user1))
+    end
+
+    xit '#sadpath - an invalid user id does not work' do
 
     end
   end
