ad98cbddff7c69f76cd7def4e791fb9aeab65c05
Miranda Delaney <delaney.miranda1@gmail.com>
2024-01-30 12:06:19 -0700

    add user poros

---
+++ app/poros/user.rb
@@ -0,0 +1,8 @@
+class User
+  attr_reader :name, :email
+  
+  def initialize(name, email)
+    @name = name
+    @email = email
+  end
+end
---
+++ spec/features/users/discover/index_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'User Discover Movies', type: :feature do
+  before(:each) do
+    
+  end
+
+ # US 1
+  describe 'When I visit the "/users/:id/discover" path' do
+    it 'I see a Button to Discover Top Rated Movies,
+      a text field to enter keyword(s) to search by movie title,
+      and a Button to Search by Movie Title' do
+
+
+    end
+
+    it '#sadpath - an invalid user id does not work' do
+
+    end
+  end
+end
+
+
